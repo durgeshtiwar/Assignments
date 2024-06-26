9d74850efe30e68666d19602b68b0a5e4334ce0d
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-06-26 19:03:37 +0530

    Selection Sort

--- Assign.cpp
+++ Assign.cpp
@@ -1,66 +1,32 @@
-// 1. Write a function to find the greatest number from the given array of any size. (TSRS)
-// 2. Write a function to find the smallest number from the given array of any size. (TSRS)
-// 3. Write a function to sort an array of any size. (TSRS)
-// 4. Write a function to rotate an array by n position in d direction. The d is an indicative
-// value for left or right. (For example, if array of size 5 is [32, 29, 40, 12, 70]; n is 2 and
-// d is left, then the resulting array after left rotation 2 times is [40, 12, 70, 32, 29] )
-// 5. Write a function to find the first occurrence of adjacent duplicate values in the array.
-// Function has to return the value of the element.
-// 6. Write a function in C to read n number of values in an array and display it in reverse
-// order.
-// 7. Write a function in C to count a total number of duplicate elements in an array.
-// 8. Write a function in C to print all unique elements in an array.
-// 9. Write a function in C to merge two arrays of the same size sorted in descending
-// order.
-// 10. Write a function in C to count the frequency of each element of an array.
-
-#include<iostream>
+#include <iostream>
 using namespace std;
-void displayData(int arr1[],int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout<<arr1[i]<<" ";
-    }
-}
-void rotateArrayLeft(int arr[],int n)
-{
-    int arr1[n] , position;
-    cout<<"Enter the position for left Rotataion : ";
-    cin>>position;
-    for (int i = 0; i < n-position; i++)
-    {
-        arr1[i]=arr[i+position];
-        
-    }
-    for (int i = 0; i < position; i++)
-    {
-        arr1[n-position+i] = arr[i];
-    }
-    displayData(arr1,5);
-}
-void rotateArrayRight(int arr[],int n)
-{
-    int arr1[n] , position;
-    cout<<"Enter the position for right Rotation : ";
-    cin>>position;
-    for (int i = 0; i < position; i++)
-    {
-        arr1[i] = arr[n-position+i];
-    }
-    for (int i = 0; i < n-position; i++)
-    {
-        arr1[i+position] = arr[i];
-        
-    }
-    displayData(arr1,5);
-}
 
+void selectionSort(int arr[], int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        int index = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[index])
+            {
+                index = j;
+            }
+        }
+        int temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i];
+    }
+}
 int main()
 {
-    int arr[5] = {32, 29, 40, 12, 70};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    rotateArrayLeft(arr,n);
-    rotateArrayRight(arr,n);
+    int arr[] = {2, 7, 5, 3, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, size);
     return 0;
 }
