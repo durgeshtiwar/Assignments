5ea307479ab4e1856f6300cbd0c0d0c80b4b606a
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-06 00:30:30 +0530

    Operator [ ] Overloading

--- Assign.cpp
+++ Assign.cpp
@@ -1,73 +1,45 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-class MyString
+class Array
 {
-    char *str;
-    int size;
+    int arr[100];
+    const int size = 100;
 
 public:
-    MyString()
+    void setArray(int index, int n)
     {
-        cout << "Default Constructor Called" << endl;
-        str = (char *)malloc(1);
-        size = 1;
+        if (index >= size)
+        {
+            cout << "Array Index out of bound Exception" << endl;
+            exit(0);
+        }
+        arr[index] = n;
     }
-    MyString(char *a)
+    int operator[](int index)
     {
-        cout << "Parameterized Constructor Called" << endl;
-        size = strlen(a);
-        str = (char *)malloc(size + 1);
-        strcpy(str, a);
+        if (index >= size)
+        {
+            cout << "Array Index out of bound Exception" << endl;
+            exit(0);
+        }
+        return arr[index];
     }
-    MyString(const MyString &a)
+    void display(int index)
     {
-        cout << "Copy Constructor Called" << endl;
-        size = strlen(a.str);
-        str = (char *)malloc(size + 1);
-        strcpy(str, a.str);
-    }
-    MyString &operator=(const MyString &a)
-    {
-        cout << "Assignment Operator Called" << endl;
-        // if (a == a.str)
-        // {
-        //     return *this;
-        // }
-        free(str);
-        size = strlen(a.str);
-        str = (char *)malloc(size + 1);
-        strcpy(str, a.str);
-        return *this;
-    }
-    void change(char *a)
-    {
-        free(str);
-        size = strlen(a);
-        str = (char *)malloc(size + 1);
-        strcpy(str, a);
-    }
-    void display()
-    {
-        cout << "String is: " << str << endl;
+        if (index >= size)
+        {
+            cout << "Array Index out of bound Exception" << endl;
+            exit(0);
+        }
+        cout << "Element which are present on given Index " << arr[index] << endl;
     }
 };
 int main()
 {
-    MyString s1("Durgesh");
-    MyString s2 = s1;
-    MyString s3;
-    MyString s4("Ayodhya");
-    MyString s5;
-    s3 = s1;
-    s4 = s1;
-    s5 = s4 = s1;
-    s1.change("Tiwari");
-    s1.display();
-    s2.display();
-    s3.display();
-    s4.display();
-    s5.display();
+    Array a1;
+    a1.setArray(5, 10);
+    a1.display(5);
+    cout << a1[5];
     return 0;
 }
