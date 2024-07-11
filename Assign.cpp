9c60792f0009b4a47006dffab9eaa2700e916f0b
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-07-11 22:19:56 +0530

    , operator

--- Assign.cpp
+++ Assign.cpp
@@ -1,29 +1,35 @@
+// Create a Coordinate class for 3 variables x,y and z and overload comma operator
+// such that when you write c3 = (c1 , c2 ) then c2 is assigned to c3. Where c1,c2,and
+// c3 are objects of 3D coordinate class.
 #include <iostream>
 using namespace std;
-
-class integer
+class Coordinate
 {
-    bool number = false;
+    int x;
+    int y;
+    int z;
 
 public:
-    bool operator!()
+    Coordinate() {}
+    Coordinate(int a, int b, int c)
     {
-        if (!number)
-            return true;
-        else
-            return false;
+        x = a;
+        y = b;
+        z = c;
+    }
+    void display()
+    {
+        cout << "Coordinate is ( " << x << "," << y << "," << z << " )";
     }
 };
 int main()
 {
-    integer i;
-    if (!i)
-    {
-        cout << "This is Well Overloading of Operator !";
-    }
-    else
-    {
-        cout << "Overloading is Fail";
-    }
+    Coordinate c1(1, 2, 3);
+    Coordinate c2(4, 5, 6);
+    Coordinate c3(7, 8, 9);
+    c3 = (c1, c2);
+    c1.display();
+    c2.display();
+    c3.display();
     return 0;
 }
