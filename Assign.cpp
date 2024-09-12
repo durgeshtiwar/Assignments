983b4058b57529e2a8f1c8f278669beeba2ed7b2
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-12 12:49:27 +0000

    change4

--- Assign.cpp
+++ Assign.cpp
@@ -1,35 +1,42 @@
 // Create a Coordinate class for 3 variables x,y and z and overload comma operator
 // such that when you write c3 = (c1 , c2 ) then c2 is assigned to c3. Where c1,c2,and
 // c3 are objects of 3D coordinate class.
+// #include <iostream>
+// using namespace std;
+// class Coordinate
+// {
+//     int x;
+//     int y;
+//     int z;
+
+// public:
+//     Coordinate() {}
+//     Coordinate(int a, int b, int c)
+//     {
+//         x = a;
+//         y = b;
+//         z = c;
+//     }
+//     void display()
+//     {
+//         cout << "Coordinate is ( " << x << "," << y << "," << z << " )";
+//     }
+// };
+// int main()
+// {
+//     Coordinate c1(1, 2, 3);
+//     Coordinate c2(4, 5, 6);
+//     Coordinate c3(7, 8, 9);
+//     c3 = (c1, c2);
+//     c1.display();
+//     c2.display();
+//     c3.display();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-class Coordinate
-{
-    int x;
-    int y;
-    int z;
-
-public:
-    Coordinate() {}
-    Coordinate(int a, int b, int c)
-    {
-        x = a;
-        y = b;
-        z = c;
-    }
-    void display()
-    {
-        cout << "Coordinate is ( " << x << "," << y << "," << z << " )";
-    }
-};
 int main()
 {
-    Coordinate c1(1, 2, 3);
-    Coordinate c2(4, 5, 6);
-    Coordinate c3(7, 8, 9);
-    c3 = (c1, c2);
-    c1.display();
-    c2.display();
-    c3.display();
-    return 0;
+    cout<<"Hello World"<<endl;
 }
