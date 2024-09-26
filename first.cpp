3ad2bee1e007747b0acd0fb9667d39ff059f834e
Durgesh Tiwari <112402246+durgeshtiwar@users.noreply.github.com>
2024-09-26 19:40:54 +0000

    pivot

--- first.cpp
+++ first.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int peakIndexInMountainArray(vector<int>& arr) {
+int getPivot(vector<int>& arr) {
     int start = 0, end = arr.size()-1;
     int mid = start + (end - start)/2;
     while (end>start)
     {
-        if (arr[mid]>=arr[0])
+        if(arr[mid]>=arr[0])
         {
             start = mid + 1;
         }else{
             end = mid;
         }
+        mid = start + (end - start)/2;
     }
     return start;
     
 }
 int main()
 {
-    vector<int>num = {12, 15, 18, 2, 4};
-    cout<<"------"<<peakIndexInMountainArray(num);
+    vector<int>num = {3,4,5,8};
+    cout<<"------"<<getPivot(num)<<endl;
     return 0;
 }
