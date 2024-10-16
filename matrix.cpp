6980263c3b3155bba30b773564dab0801cbf442e
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-10-16 18:14:04 +0530

    String getline

--- matrix.cpp
+++ matrix.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 int main()
 {
-  vector<vector<int>> vect1(4, vector<int>(4, 1));
-  cout << "Enter the no." << endl;
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      cin >> vect1[i][j];
-    }
-  }
-  cout << endl;
-  for (int i = 0; i < 4; i++)
-  {
-    for (int j = 0; j < 4; j++)
-    {
-      if (i % 2 == 0)
-      {
-        cout << vect1[j][i] << " ";
-      }
-      else
-      {
-        cout << vect1[3 - j][i] << " ";
-      }
-    }
-    cout << endl;
-  }
-
+  string s;
+  getline(cin, s);
+  cout << s;
   return 0;
 }
