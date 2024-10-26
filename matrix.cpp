cc1263311b4ea4e79c50975eafed3a7bad326da3
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2024-10-26 09:32:56 +0530

    container most water

--- matrix.cpp
+++ matrix.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 int main()
 {
-  string s;
-  getline(cin, s);
-  cout << s;
+  vector<int> vect2 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+  int maxContainer = INT32_MIN;
+  for (int i = 0; i < vect2.size(); i++)
+  {
+    for (int j = i + 1; j < vect2.size(); j++)
+    {
+      int w = j - i;
+      int h = min(vect2[i], vect2[j]);
+      maxContainer = max(maxContainer, w * h);
+    }
+  }
+  cout << "Maximum container is = " << maxContainer;
   return 0;
 }
