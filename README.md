4e8c664e364a5016f586a14565bd30afe2dc6d54
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2025-05-30 20:52:00 +0530

    30 July

---
+++ README.md
@@ -0,0 +1,9 @@
+Power BI Upto 10
+Excel
+SQL and MYSQL
+Python
+
+Power BI
+
+1. Maps, pie charts, complete basics.
+2. introduction
--- matrix.cpp
+++ matrix.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+class Complex
+{
+private:
+  int num1, num2;
+
+public:
+  void setData(int num1, int num2)
+  {
+    int num1 = num1;
+    int num2 = num2;
+  }
+  void showData()
+  {
+    cout << "Real Number is = " << num1 << endl;
+    cout << "Image Number is = " << num2 << endl;
+  }
+};
 int main()
 {
-  vector<int> vect2 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-  int maxContainer = INT32_MIN;
-  for (int i = 0; i < vect2.size(); i++)
-  {
-    for (int j = i + 1; j < vect2.size(); j++)
-    {
-      int w = j - i;
-      int h = min(vect2[i], vect2[j]);
-      maxContainer = max(maxContainer, w * h);
-    }
-  }
-  cout << "Maximum container is = " << maxContainer;
-  return 0;
+  Complex c1, c2;
+  c1.setData(3, 5);
+  c2.setData(4, 5);
+  c1.showData();
+  c2.showData();
 }
