be2e1df5babb476fe289b36960f10704d6184405
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2026-04-01 23:14:16 +0530

    Remove unused configuration files and add Python warm-up assignment

---
+++ Warm-up with python/Solution.py
@@ -0,0 +1,12 @@
+# import sys
+# print(sys.version)
+
+# print("Hello Python")
+# print("Hello \n Pyhton")
+# print('"MysirG"')
+# print("\"Happy Teacher's day\"")
+# Name = "Durgesh Pratap Tiwari"
+# age = 22.5
+# qualification = "B.Tech"
+# YOE = "1.5 Year"
+# print(Name,"\n",age,"\n",qualification,"\n",YOE)
