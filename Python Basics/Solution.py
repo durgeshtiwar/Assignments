edee8f6402463ec32229bf5197e02b17bd3eafcf
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2026-04-03 23:45:19 +0530

    Uncomment Python code examples for learning variables, types, and datetime

--- Python Basics/Solution.py
+++ Python Basics/Solution.py
@@ -1,2 +1,37 @@
 # print("Comments")
-print("Learning Python")
+# print("Learning Python")
+
+# var_1 = 35
+# var_2 = True
+# var_3 = "MySirG"
+# var_4 = 5.46
+# var_5 = 3+4j
+
+# print(type(var_1))
+# print(type(var_2))
+# print(type(var_3))
+# print(type(var_4))
+# print(type(var_5))
+
+# var_1 = 10
+# var_2 = 10
+# var_3 = 20
+
+# print(id(var_1))
+# print(id(var_2))
+# print(id(var_3))
+
+# import keyword
+# print(keyword.kwlist)
+
+# var_1 = def
+# print(var_1)
+
+# from datetime import datetime
+
+# var_1 = datetime.now()
+# print(var_1)
+# var_2 = var_1.date()
+# var_3 = var_1.time()
+# print(var_2)
+# print(var_3)
