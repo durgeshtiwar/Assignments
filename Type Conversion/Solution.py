bd30d5d1ef23f54537b867ad24fd1ddb369fa5e8
Durgesh tiwari <durgeshtiwari362592@gmail.com>
2026-04-08 23:06:25 +0530

    Add Python scripts for type conversion exercises

---
+++ Type Conversion/Solution.py
@@ -0,0 +1,14 @@
+# var_int = 2026
+# var_str = str(var_int)
+# print(var_int)
+# print(type(var_int))
+# print(var_str)
+# print(type(var_str))
+
+# print(ord('m'))
+print(chr(100))
+var_1 = 65
+var_2 = 100
+print("Given no. is",var_1)
+print("Unicode of",var_1,"is",chr(var_1))
+print("Binary of", var_2,"is",bin(var_2))
